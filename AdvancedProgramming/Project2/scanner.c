81f926f715fcc27b1a2713c2603038e4706f680d
diana ramirez <ramid320@gmail.com>
2020-02-29 09:51:38 -0800

    updated lexical errors to display line #

--- AdvancedProgramming/Project2/scanner.c
+++ AdvancedProgramming/Project2/scanner.c
@@ -3,17 +3,21 @@
 #include <string.h>
 #include "ctype.h"
 
+int lineNum =1; //counter for line number
+int mStart;
 typedef enum {read, write, id, literal, becomes,
                 addOp, subOp, mulOp, divOp, lparen, rparen, eof} token;
 
 char token_image[100];
 
 FILE *fileSrc;
+char buffer[80];
 
 void setSource(FILE *src)
 {
     fileSrc = src;
 }
+
 
 int getNext()
 {
@@ -21,9 +25,15 @@ int getNext()
 
     if (fileSrc == NULL)
         c = getchar();
-    else
+    else {
         c = fgetc(fileSrc);
-    
+        if (c == '\n')
+        {
+            ++lineNum;
+        }
+    }
+
+
     // printf("next char: %c\n", c);
     return c;
 }
@@ -32,7 +42,9 @@ token scan() {
     static int c = ' ';
         /* next available char; extra (int) width accommodates EOF */
     int i = 0;              /* index into token_image */
-
+    //int x;
+    //x = endofline(fileSrc, lineNum);
+    //printf("x= %d \n",x);
     /* skip white space */
     while (isspace(c)) {
         c = getNext();
@@ -59,7 +71,7 @@ token scan() {
     } else switch (c) {
         case ':':
             if (c = getNext() != '=') {
-                fprintf(stderr, "lexical error\n");
+                fprintf(stderr, "lexical error on line #%d\n",lineNum);
                 exit(1);
             } else {
                 c = getNext();
@@ -69,17 +81,18 @@ token scan() {
         case '+': c = getNext(); return addOp;
         case '-': c = getNext(); return subOp;
         case '*': c = getNext(); return mulOp;
-        case '/': 
+        case '/':
             c = getNext();
             switch (c) {
                 case '/':
-                    printf("line comment found\n");
+                    printf("line comment found on line #%d\n",lineNum);
                     do {
                         c = getNext();
                     }while(c != '\n');
                     c = getNext();
                     return scan();
                 case '*':
+                    mStart=lineNum;
                     do {
                         c = getNext();
                         if(c == '*') {
@@ -90,7 +103,7 @@ token scan() {
                             }
                         }
                     }while(c != EOF);
-                    printf("lexical error: invalid multi-line comment\n");
+                    printf("lexical error: invalid multi-line comment starting on line #%d\n",mStart);
                     exit(1);
                 default:
                     return divOp;
@@ -98,7 +111,7 @@ token scan() {
         case '(': c = getNext(); return lparen;
         case ')': c = getNext(); return rparen;
         default:
-            printf("lexical error\n");
+            printf("lexical error on line #%d\n",lineNum);
             exit(1);
     }
 }
